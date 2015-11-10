75284412c3ffe9f41f38c4f18a301a72f078fac2
Bas Broerse <basbroerse@hotmail.com>
2015-11-10 10:16:38 +0100

    Updated gitignore

--- src/FastenYourSeatbelts/FastenYourSeatbelts.java
+++ src/FastenYourSeatbelts/FastenYourSeatbelts.java
@@ -23,12 +23,8 @@ public class FastenYourSeatbelts extends Application {
     public void start(Stage primaryStage) {
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
+        btn.setOnAction((ActionEvent event) -> {
+            System.out.println("Hello World!");
         });
         
         StackPane root = new StackPane();
