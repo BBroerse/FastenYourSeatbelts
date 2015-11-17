be015ca9bd26894e9ca64fa90d6c36cb434a1297
jandorres <jan@base.nl>
2015-11-17 11:04:21 +0100

    basis

---
+++ src/application/Main.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author jandorresteijn
+ */
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        BorderPane root = FXMLLoader.load(getClass().getResource("/views/main.fxml"));
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
