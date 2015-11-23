6a23881d212bb025a578502f681f198efe07c50a
Bas Broerse <basbroerse@hotmail.com>
2015-11-23 16:36:36 +0100

    shit

--- src/application/Main.java
+++ src/application/Main.java
@@ -25,7 +25,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
-            BorderPane root = FXMLLoader.load(getClass().getResource("/views/main.fxml"));
+            BorderPane root = FXMLLoader.load(getClass().getResource("/views/Main.fxml"));
             primaryStage.setScene(new Scene(root, 900, 800));
             root.getStylesheets().add("/default.css");
             primaryStage.show();
